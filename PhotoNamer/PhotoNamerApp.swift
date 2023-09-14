44a431d486b406ef602592b3266178d03699d940
Eugene Cross <eugene.cross@gmail.com>
2023-09-14 15:13:09 +0100

    Initial Commit

---
+++ PhotoNamer/PhotoNamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoNamerApp.swift
+//  PhotoNamer
+//
+//  Created by Eugene on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoNamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
