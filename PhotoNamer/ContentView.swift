9836bbad7e10adfa3e9b79209defd47f3a2f1454
Eugene Cross <eugene.cross@gmail.com>
2023-09-17 21:44:06 +0100

    Refactor select view from content to PhotoSelectView, using @Binding

--- PhotoNamer/ContentView.swift
+++ PhotoNamer/ContentView.swift
@@ -12,8 +12,7 @@ struct ContentView: View {
     @State var photo: Photo
     @State private var showList = true
     @StateObject private var photos = Photos()
-    @State private var image: Image?
-    @State private var showPhotoPicker = false
+   
     
     let savePath = FileManager.documentsDirectory.appendingPathComponent("PhotoNamerData.json")
    
@@ -24,40 +23,7 @@ struct ContentView: View {
                 if showList {
                     PhotoListView()
                 } else {
-                    VStack {
-                        ZStack {
-                            Rectangle()
-                                .fill(Gradient(colors: [.yellow, .red]))
-
-                            Text("Tap to select a picture")
-                                .foregroundColor(.white)
-                                .font(.headline)
-
-                            image?
-                                .resizable()
-                                .scaledToFit()
-
-                        }
-                        .onTapGesture {
-                            image = nil
-                            photo.photoName = ""
-                            showPhotoPicker = true
-                        }
-                        .sheet(isPresented: $showPhotoPicker) {
-                            PhotoPicker(image: $photo.inputImage)
-                        }
-                        .onChange(of: photo.inputImage) { _ in loadImage() }
-
-
-                        VStack (alignment: .leading, spacing: 0.2) {
-
-                            TextField("Enter a name", text: $photo.photoName)
-                                .padding(.bottom)
-                                .disabled(image == nil)
-                            Spacer()
-                        }
-                        .padding()
-                    }
+                    PhotoSelectView(photo: $photo)
                 }
                 
             }
@@ -84,11 +50,7 @@ struct ContentView: View {
         }
     }
     
-    func loadImage() {
-        guard let inputImage = photo.inputImage else { return }
-                
-        image = Image(uiImage: inputImage)
-    }
+
     
     func save() {
         do {
--- PhotoNamer/PhotoSelectView.swift
+++ PhotoNamer/PhotoSelectView.swift
@@ -5,60 +5,63 @@
 //  Created by Eugene on 14/09/2023.
 //
 
-//import SwiftUI
+import SwiftUI
 
-//struct PhotoSelectView: View {
+struct PhotoSelectView: View {
     
-//    @StateObject private var photo = Photo()
-//    @State private var image: Image?
-//    @State private var showPhotoPicker = false
+    @Binding var photo: Photo
+    
+    @State private var image: Image?
+    @State private var showPhotoPicker = false
 
     
-//    var body: some View {
-//        VStack {
-//            ZStack {
-//                Rectangle()
-//                    .fill(Gradient(colors: [.yellow, .red]))
-//
-//                Text("Tap to select a picture")
-//                    .foregroundColor(.white)
-//                    .font(.headline)
-//
-//                image?
-//                    .resizable()
-//                    .scaledToFit()
-//
-//            }
-//            .onTapGesture {
-//                showPhotoPicker = true
-//            }
-//            .sheet(isPresented: $showPhotoPicker) {
-//                PhotoPicker(image: $photo.inputImage)
-//            }
-//            .onChange(of: photo.inputImage) { _ in loadImage() }
-//
-//
-//            VStack (alignment: .leading, spacing: 0.2) {
-//
-//                TextField("Enter a name", text: $photo.photoName)
-//                    .padding(.bottom)
-//                    .disabled(image == nil)
-//                Spacer()
-//            }
-//            .padding()
-//        }
+    var body: some View {
+        VStack {
+            ZStack {
+                Rectangle()
+                    .fill(Gradient(colors: [.yellow, .red]))
+
+                Text("Tap to select a picture")
+                    .foregroundColor(.white)
+                    .font(.headline)
+
+                image?
+                    .resizable()
+                    .scaledToFit()
+
+            }
+            .onTapGesture {
+                image = nil
+                photo.photoName = ""
+                showPhotoPicker = true
+            }
+            .sheet(isPresented: $showPhotoPicker) {
+                PhotoPicker(image: $photo.inputImage)
+            }
+            .onChange(of: photo.inputImage) { _ in loadImage() }
+
+
+            VStack (alignment: .leading, spacing: 0.2) {
+
+                TextField("Enter a name", text: $photo.photoName)
+                    .padding(.bottom)
+                    .disabled(image == nil)
+                Spacer()
+            }
+            .padding()
+        }
         
-//    }
+    }
     
-//    func loadImage() {
-//        guard let inputImage = photo.inputImage else { return }
-//
-//        image = Image(uiImage: inputImage)
-//    }
-//}
+    func loadImage() {
+        guard let inputImage = photo.inputImage else { return }
+                
+        image = Image(uiImage: inputImage)
+    }
+}
 
-//struct PhotoSelectView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        PhotoSelectView()
-//    }
-//}
+struct PhotoSelectView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoSelectView(photo: .constant(Photo(id: UUID()   )))
+    }
+}
